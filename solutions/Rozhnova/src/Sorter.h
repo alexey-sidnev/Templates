892b0f0edb8287002a28810b88938e6d928c52c3
unknown <stud2015@ws-2k-110-n12.ccam.cmc.unn.net>
2015-10-23 16:17:35 +0300

    done

---
+++ solutions/Rozhnova/src/Sorter.h
@@ -0,0 +1,42 @@
+#pragma once
+
+template <class Type>
+class Sorter 
+{
+public:
+	void static sort (Type* mas, int size)
+	{
+		Type tmp = 0;
+		int i, j;
+		for (i = 0; i < size; i++)
+		{
+			for (j = i+1; j < size; j++)
+			{
+				if (compare (mas[j], mas[i])) {
+				tmp = mas[i];
+				mas[i] = mas[j];
+				mas[j] = tmp;
+				}
+			}
+		}	
+	}
+};
+
+
+template <class Type>
+int compare (Type a, Type b)
+{
+	if (a < b)
+		return 1;
+	return 0;
+}
+
+template <class Type>
+int compare (Complex<Type> a, Complex <Type> b)
+{
+	if (a.re_ < b.re_ ) return 1;
+	else 
+		if ((a.re_ == b.re_) && (a.im_ < b.im_))
+			return 1;
+		else return 0;
+}
