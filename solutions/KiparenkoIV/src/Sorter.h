64b26cd80eaf173f297fb2f09db4a9762b691e5d
IlyaKiparenko <ilya.kiparenko@gmail.com>
2015-10-23 19:56:36 +0400

    Test1

---
+++ solutions/KiparenkoIV/src/Sorter.h
@@ -0,0 +1,48 @@
+
+#include "Complex.h"
+
+
+
+
+
+template<typename Type>
+ int comp(Type& a,Type& b)
+	{
+		return a>b;
+	}
+  template <class Type>
+	 int comp(Complex<Type>& a,Complex<Type>& b)
+	{
+		return (comp(a.im_,b.im_));//)||comp(a.re_,b.re_));
+	}
+
+
+
+
+
+
+
+template <class Type>
+class Sorter
+{public:
+	static void sort(Type* mas,int size)
+	{
+		Type t;
+		for(int i=0; i<size; i++)
+		{
+			for(int j=i+1; j<size; j++)
+			{
+				if(comp(mas[i],mas[j]))
+				{
+					t=mas[i];
+					mas[i]=mas[j];
+					mas[j]=t;
+					
+				}
+
+			}
+		}
+
+	}
+};
+
