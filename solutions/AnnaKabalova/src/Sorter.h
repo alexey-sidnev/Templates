5bc293075f1e73d773e73da04ad85e7b9dc4c968
AnnaKabalova <kabalova1@gmail.com>
2015-10-23 15:57:15 +0300

    Temp

---
+++ solutions/AnnaKabalova/src/Sorter.h
@@ -0,0 +1,39 @@
+#pragma once
+template <class Type>
+
+bool comp(Type a1, Type a2)
+{
+	if (a1>a2) return true;
+	else return false;
+}
+
+template <class Type>
+bool comp(Complex<Type> a1, Complex<Type> a2)
+{
+	if (a1.re_>a2.re_) 
+		return true;
+	else 
+		if (a1.re_==a2.re_)
+			if (a1.im_>a2.im_)
+				return true;
+	return false;
+
+}
+template <class Type>
+class Sorter
+{
+public:
+	void static sort(Type* a,int n)
+	{
+		for (int i=0;i<n;i++)
+			for (int j=i+1;j<n;j++)
+				if (comp(a[i],a[j]))
+				{
+					Type tmp;
+					tmp=a[i];
+					a[i]=a[j];
+					a[j]=tmp;
+				}
+	};
+};
+
