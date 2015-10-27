2e98655b3516a922474c00da0f399d22929cce62
Zhorin Sergey <ser4038772@mail.ru>
2015-10-27 20:00:47 +0300

    И еще пригоршня пробелов

--- solutions/Zhorin/src/Sorter.h
+++ solutions/Zhorin/src/Sorter.h
@@ -9,46 +9,43 @@ template <class Type>
 class Sorter{
  public:
   static void sort(Type*a, int n) {
-   for (int i = 0; i < n; i++)
-    for (int j = i + 1; j < n; j++)
-     if (MyCompare(a[i], a[j])){
-      Type tmp = a[i];
-      a[i] = a[j];
-      a[j] = tmp;
+    for (int i = 0; i < n; i++)
+     for (int j = i + 1; j < n; j++)
+      if (MyCompare(a[i], a[j])){
+       Type tmp = a[i];
+       a[i] = a[j];
+       a[j] = tmp;
 
-		}
-	}
+      }
+   }
 };
 
 template<typename Type>
-bool MyCompare(Type &a, Type &b) {
- if (a > b)
-  return true;
- return false;
+bool MyCompare(const Type &a, const Type &b) {
+  if (a > b)
+   return true;
+  return false;
 }
-
 template<class Type>
-bool MyCompare(Complex<Type> &a, Complex<Type> &b) {
- if (a.re_ > b.re_)
-  return true;
- if (a.re_ == b.re_ && a.im_ > b.im_)
-  return true;
- return false;
+bool MyCompare(const Complex<Type> &a, const Complex<Type> &b) {
+  if (a.re_ > b.re_)
+   return true;
+  if (a.re_ == b.re_ && a.im_ > b.im_)
+   return true;
+  return false;
 }
-
 template<class Type>
-bool Eq(Complex<Type> &a, Complex<Type> &b) {
- if (a.re_ == b.re_ && a.im_ == b.im_)
-  return true;
- return false;
+bool Eq(const Complex<Type> &a, const Complex<Type> &b) {
+  if (a.re_ == b.re_ && a.im_ == b.im_)
+   return true;
+  return false;
 }
-
 template<class Type>
-bool MyCompare(Complex<Complex<Type>> &a, Complex<Complex<Type>> &b) {
- if (MyCompare(a.re_, b.re_))
-  return true;
- if (Eq(a.re_, b.re_) && MyCompare(a.im_, b.im_))
-  return true;
- return false;
+bool MyCompare(const Complex<Complex<Type>> &a, const Complex<Complex<Type>> &b) {
+  if (MyCompare(a.re_, b.re_))
+   return true;
+  if (Eq(a.re_, b.re_) && MyCompare(a.im_, b.im_))
+   return true;
+  return false;
 }
 #endif  // SOLUTIONS_ZHORIN_SRC_SORTER_H_
