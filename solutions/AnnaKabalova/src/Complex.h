572de6c323875f97445b7edb11ddccc5efbfdfe3
AnnaKabalova <kabalova1@gmail.com>
2015-10-27 21:38:38 +0300

    s5

--- solutions/AnnaKabalova/src/Complex.h
+++ solutions/AnnaKabalova/src/Complex.h
@@ -11,14 +11,14 @@ class Complex {
  public:
   Type re_, im_;
 
- explicit Complex(Type re = Type(0), Type im = Type(0)) : re_(re), im_(im)
- {}
+  explicit Complex(Type re = Type(0), Type im = Type(0)) : re_(re), im_(im)
+  {}
 };
 
 template <class Type>
 std::ostream& operator<<(std::ostream& o, const Complex<Type>& instance) {
- o << "<" << instance.re_ << ", " << instance.im_ << ">";
- return o;
+  o << "<" << instance.re_ << ", " << instance.im_ << ">";
+  return o;
 }
 
 #endif  // SOLUTIONS_ANNAKABALOVA_SRC_COMPLEX_H_
