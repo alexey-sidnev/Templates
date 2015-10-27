16b45c5e55677000a86bb6ab01db70deb34a9925
IlyaKiparenko <ilya.kiparenko@gmail.com>
2015-10-27 20:09:47 +0400

    Update Sorter.h

--- solutions/KiparenkoIV/src/Sorter.h
+++ solutions/KiparenkoIV/src/Sorter.h
@@ -6,29 +6,29 @@
 
 template<typename Type>
 int comp(const Type& a, const Type& b) {
-  return a>b;
+  return a > b;
 }
 template <class Type>
 int comp(const Complex<Type>& a, const Complex<Type>& b) {
-  return (comp(a.re_, b.re_)); 
+  return (comp(a.re_, b.re_));
 }
 
 template <class Type>
-class Sorter
-{
+class Sorter {
  public:
   static void sort(Type* mas, int size) {
     Type t;
-    for(int i = 0; i < size; i++) {
-      for(int j = i+1; j < size; j++) {
+    for (int i = 0; i < size; i++) {
+      for (int j = i+1; j < size; j++) {
 	if (comp(mas[i], mas[j])) {
-	  t = mas[i];
-	  mas[i] = mas[j];
-	  mas[j] = t;
-	}
+          t = mas[i];
+          mas[i] = mas[j];
+          mas[j] = t;
+        }
+       }
       }
     }
   }
 };
 
-#endif // SOLUTIONS_KIPARENKOIV_SRC_SORTER_H_
+#endif  // SOLUTIONS_KIPARENKOIV_SRC_SORTER_H_
