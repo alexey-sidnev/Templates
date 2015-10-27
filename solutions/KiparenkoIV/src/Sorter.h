b686f73341559248a5853ee4d791578b0eb3d0f7
IlyaKiparenko <ilya.kiparenko@gmail.com>
2015-10-27 20:05:02 +0400

    Update Sorter.h

--- solutions/KiparenkoIV/src/Sorter.h
+++ solutions/KiparenkoIV/src/Sorter.h
@@ -1,48 +1,34 @@
 
+#ifndef SOLUTIONS_KIPARENKOIV_SRC_SORTER_H_
+#define SOLUTIONS_KIPARENKOIV_SRC_SORTER_H_
+
 #include "Complex.h"
 
-
-
-
-
 template<typename Type>
- int comp(Type& a,Type& b)
-	{
-		return a>b;
-	}
-  template <class Type>
-	 int comp(Complex<Type>& a,Complex<Type>& b)
-	{
-		return (comp(a.re_,b.re_));//)||comp(a.re_,b.re_));
-	}
-
-
-
-
-
-
+int comp(const Type& a, const Type& b) {
+  return a>b;
+}
+template <class Type>
+int comp(const Complex<Type>& a, const Complex<Type>& b) {
+  return (comp(a.re_, b.re_)); 
+}
 
 template <class Type>
 class Sorter
-{public:
-	static void sort(Type* mas,int size)
-	{
-		Type t;
-		for(int i=0; i<size; i++)
-		{
-			for(int j=i+1; j<size; j++)
-			{
-				if(comp(mas[i],mas[j]))
-				{
-					t=mas[i];
-					mas[i]=mas[j];
-					mas[j]=t;
-
-				}
-
-			}
-		}
-
+{
+ public:
+  static void sort(Type* mas, int size) {
+    Type t;
+    for(int i = 0; i < size; i++) {
+      for(int j = i+1; j < size; j++) {
+	if (comp(mas[i], mas[j])) {
+	  t = mas[i];
+	  mas[i] = mas[j];
+	  mas[j] = t;
 	}
+      }
+    }
+  }
 };
 
+#endif // SOLUTIONS_KIPARENKOIV_SRC_SORTER_H_
