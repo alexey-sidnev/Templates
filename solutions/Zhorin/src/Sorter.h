0dc710e9b9323b7708190fce5504507399e51216
Zhorin Sergey <ser4038772@mail.ru>
2015-10-27 19:14:57 +0300

    Надеюсь, что мой coding style не весьма спецефичен

--- solutions/Zhorin/src/Sorter.h
+++ solutions/Zhorin/src/Sorter.h
@@ -1,15 +1,20 @@
+#pragma once
+
+#ifndef SORTER_H
+#define SORTER_H
+
 #include "Complex.h"
 
 template <class Type>
 class Sorter{
-public:
-	void static sort(Type*a,int n){
-		for (int i=0; i<n;i++)
-			for(int j=i+1;j<n;j++)
-				if(MyCompare(a[i],a[j])){
-					Type tmp=a[i];
-					a[i]=a[j];
-					a[j]=tmp;
+ public:
+  void static sort(Type*a,int n){
+	for (int i=0; i<n;i++)
+		for(int j=i+1;j<n;j++)
+			if(MyCompare(a[i],a[j])){
+				Type tmp=a[i];
+				a[i]=a[j];
+				a[j]=tmp;
 
 				}
 	}
@@ -17,16 +22,33 @@ public:
 
 template<typename Type>
 bool MyCompare(Type &a, Type &b){
-	if(a>b)
+ if(a>b)
+	return true;
+ return false;
+}
+
+template<class Type>
+bool MyCompare(Complex<Type> &a, Complex<Type> &b){
+ if(a.re_>b.re_)
+	return true;
+ if (a.re_==b.re_ && a.im_>b.im_)
+	return true;
+ return false;
+}
+
+template<class Type>
+bool Eq(Complex<Type> &a, Complex<Type> &b){
+	if (a.re_ == b.re_ && a.im_==b.im_)
 		return true;
 	return false;
 }
 
 template<class Type>
-bool MyCompare(Complex<Type> &a, Complex<Type> &b){
-	if(a.re_>b.re_)
+bool MyCompare(Complex<Complex<Type>> &a, Complex<Complex<Type>> &b){
+	if (MyCompare(a.re_,b.re_))
 		return true;
-	if (a.re_==b.re_ && a.im_>b.im_)
+	if (Eq(a.re_, b.re_) && MyCompare(a.im_, b.im_))
 		return true;
 	return false;
 }
+#endif
