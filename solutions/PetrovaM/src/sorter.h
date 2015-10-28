5c9494229e70b2fec2a2de08a4a97c71fef9c3de
mariaPetrova18912 <maria18912@yandex.ru>
2015-10-29 01:29:53 +0300

    TEST 1

---
+++ solutions/PetrovaM/src/sorter.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "Complex.h"
+
+template <class Type>
+
+bool myCompare(Type a, Type b) {
+	if (a > b)
+		return true;
+	return false;
+}
+
+template <class Type>
+bool myCompare(Complex<Type> a, Complex<Type> b) {
+	if (a.re_ > b.re_)
+		return true;
+	if (a.re_ == b.re_)
+	 if (a.im_ > b.im_)
+		return true;
+	return false;
+}
+
+template <class Type>
+class Sorter {
+ public:
+	static void sort(Type *a, int size) {
+		for(int i = 0; i < size; i++)
+			for(int j = i + 1; j < size; j++)
+				if (myCompare(a[i],a[j]))
+				{
+					Type tmp = a[i];
+					a[i] = a[j];
+					a[j] = tmp;
+				}
+	}
+};
